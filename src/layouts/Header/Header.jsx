4ef7a44e7e9e4601a159092e1fc3e771ff06d905
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2025-08-20 23:16:17 +0300

    #1 Верстаем макет с нуля | JSX + Minista + Vite (LIVE)

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,47 @@
+import './Header.scss'
+import clsx from 'clsx'
+import Logo from '@/components/Logo'
+import Socials from '@/components/Socials'
+import BurgerButton from '@/components/BurgerButton'
+
+export default (props) => {
+  const { url } = props
+
+  const menuItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Recipes', href: '/recipes' },
+    { label: 'Blog', href: '/blog' },
+    { label: 'Contact', href: '/contact' },
+    { label: 'About us', href: '/about' },
+  ]
+
+  return (
+    <header className="header" data-js-mobile-menu="">
+      <div className="header__inner container">
+        <Logo className="header__logo" />
+        <nav className="header__menu" data-js-mobile-menu-overlay="">
+          <ul className="header__menu-list">
+            {menuItems.map(({ label, href }) => (
+              <li className="header__menu-item">
+                <a
+                  className={clsx(
+                    'header__menu-link',
+                    href === url && 'is-active'
+                  )}
+                  href={href}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Socials className="header__soc1als" />
+        <BurgerButton
+          className="header__burger-button visible-mobile"
+          extraAttrs={{ 'data-js-mobile-menu-burger-button': '' }}
+        />
+      </div>
+    </header>
+  )
+}
